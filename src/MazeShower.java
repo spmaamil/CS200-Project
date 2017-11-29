4d6f4f8d0fa4017dda671565afb7a24f2ef0936e
Nathan Miller <njmrocker@gmail.com>
2017-11-29 10:46:42 -0600

    draw maze on panel

--- src/MazeShower.java
+++ src/MazeShower.java
@@ -1,74 +1,51 @@
-import javax.swing.*;
-import java.awt.*;
+	import javax.swing.*;
+	import java.awt.*;
 
-public class MazeShower extends JPanel implements Runnable
-{
+	public class MazeShower extends JPanel
+	{
+	     RandomMaze r = new RandomMaze(25,25);
+	     public static void main(String args[])
+	     {
+	          JFrame frame = new JFrame("Maze creater, and Solver");
+	          MazeShower maze = new MazeShower();
+	          frame.setPreferredSize(new Dimension(500,500));
+	          frame.setLayout(new BorderLayout());
+	          
+	          frame.add(maze,BorderLayout.CENTER);
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.pack();
+	          frame.setVisible(true);
+	     }    
+	     public MazeShower()
+	     { 
+	          this.setSize(500,500);                 
+	     }
 
-     private Thread thread;
-     private boolean suspended;
-     public static void main(String args[])
-     {
-          JFrame frame = new JFrame("Maze creater, and Solver");
-          MazeShower maze = new MazeShower();
-          frame.setPreferredSize(new Dimension(500,500));
-          frame.setLayout(new BorderLayout());
-          
-          frame.add(maze,BorderLayout.CENTER);
-          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frame.pack();
-          frame.setVisible(true);
-          maze.start();
-//          maze.suspend();
-     }    
-     public MazeShower()
-     { 
-          this.setSize(500,500);                 
-     }
-
-     public void start()
-     {
-          if(thread == null)
-               thread = new Thread(this);
-          thread.start();          
-     }
-
-     public void suspend()
-     {
-          suspended = true;
-     }
-     public synchronized void resume()
-     {
-          suspended = false;
-          notify();
-     }
-     public void paintComponent(Graphics g)
-     {
-          int rnd = (int)(Math.random() * 256); 
-          g.setColor(new Color(rnd % 200, rnd % 100, rnd));
-          g.fillRect(0,0,500,500);
-     }
-
-     public void run()
-     {
-          suspended = false;
-          
-
-           while(true)
-           {
-               synchronized(this)
-               {
-                   try
-                   {
-//                       System.out.println(suspended);
-                       this.repaint();
-                       while(suspended)
-                           wait();
-                   }
-                   catch(Exception e)
-                   {
-                   }
-               } 
-          }
-
-     }
-}
+	     public void paintComponent(Graphics g)
+	     {
+	    	 int y = 0;
+	    	 int x = 0;
+	          int rnd = (int)(Math.random() * 256); 
+	          g.setColor(new Color(rnd % 200, rnd % 100, rnd));
+	          for (String line: r.toString().split("\n"))
+	          {
+	        	  String[] charArray = line.split("");
+	        	  for (String c : charArray)
+	        	  {
+	        		  if(c.equals("-"))
+	        			g.setColor(Color.BLACK);
+	        			    
+	        		  else if(c.equals(" "))
+	        			  g.setColor(Color.WHITE);
+	        			  
+	        			  
+	        		g.fillRect(x+ 10, y + 10, 10, 10);
+	        			
+	        		x+= 5;
+	        	  }
+	        	  y += 10;
+	        	  x = 0;
+	          }
+	      System.out.println(r);    
+	     }
+	}
